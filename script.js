dad7d1c306b5467c98748416ef103077723449b2
Gus <gusrichardson1@gmail.com>
2019-05-02 15:19:08 -0400

    added russian translation - rough

--- script.js
+++ script.js
@@ -25,4 +25,32 @@ $('document').ready(function () {
         })
     })
 
+    //RUSSIAN MENU BUTTONS
+    $('#russian-show-food').on("click", function () {
+        $('#russian-drinks').css({
+            display: "none"
+        })
+        $('#russian-food').css({
+            display: 'block'
+        })
+    })
+    $('#russian-show-drinks').on("click", function () {
+        $('#russian-food').css({
+            display: "none"
+        })
+        $('#russian-drinks').css({
+            display: 'block'
+        })
+    })
+
+    $('.translate').on('click', function () {
+        $('.english').toggleClass('display-none');
+        $('.russian').toggleClass('display-none');
+    })
+
+    // $('.translate-english').on('click', function () {
+    //     $('.russian').toggleClass('display-none');
+    //     $('.english').toggleClass('display-none');
+    // })
+
 })
