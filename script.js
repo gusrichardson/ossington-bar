37e3c8f5a8efce3dad38a3a96c820426265acd94
Gus <gusrichardson1@gmail.com>
2019-05-10 08:33:22 -0400

    rought draught mobile nav

--- script.js
+++ script.js
@@ -47,50 +47,15 @@ $('document').ready(function () {
         $('.russian').toggleClass('display-none');
     })
 
-    //PARALLAX
-    $(window).scroll(function () {
-        let windowScroll = $(this).scrollTop();
-        console.log(windowScroll);
-        // $('.welcome-text').css({
-        //     transform: `translate(0px, ${windowScroll / 5}%)`
-        // })
-        // $('#letter-1').css({
-        //     transform: `translate(0px, ${windowScroll}%) rotate(-${windowScroll / 4}deg`
-        // })
-        // $('#letter-2').css({
-        //     transform: `translate(0px, ${windowScroll / 2}%) rotate(-${windowScroll / 5}deg)`
-        // })
-        // $('#letter-3').css({
-        //     transform: `translate(0px, ${windowScroll / 3}%) rotate(-${windowScroll / 6}deg)`
-        // })
-        // $('#letter-4').css({
-        //     transform: `translate(0px, ${windowScroll / 4}%)`
-        // })
-        // $('#letter-5').css({
-        //     transform: `translate(0px, ${windowScroll / 5}%)`
-        // })
-        // $('#letter-6').css({
-        //     transform: `translate(0px, ${windowScroll / 4}%)`
-        // })
-        // $('#letter-7').css({
-        //     transform: `translate(0px, ${windowScroll / 3}%) rotate(${windowScroll / 6}deg)`
-        // })
-        // $('#letter-8').css({
-        //     transform: `translate(0px, ${windowScroll / 2}%) rotate(${windowScroll / 5}deg)`
-        // })
-        // $('#letter-9').css({
-        //     transform: `translate(0px, ${windowScroll}%) rotate(${windowScroll / 4}deg)`
-        // })
-
-        $('.shutter').css({
-            transform: `translate(0px, -${windowScroll / 10}%)`
+    $('#side-nav-button').on('click', function () {
+        $('.header-nav').css({
+            width: '100%',
         })
-        // $('.welcome-text').css({
-        //     transform: `translate(0px, ${windowScroll / 4}%)`
-        // })
-        // $('#parallax-logo').css({
-        //     transform: `translate(${windowScroll / 4}%, -${windowScroll / 1.5}%) rotate(${windowScroll / 4}deg)`
-        // })
+    })
+    $('#close-menu').on('click', function () {
+        $('.header-nav').css({
+            width: '0'
+        })
     })
 
 })
